55563cabd9cc9d86cfa81237c3e6276554007e83
abdel el akroudi <lsroudi@gmail.com>
2013-12-23 17:00:43 +0100

    function and exception in JavaScript

---
+++ Exception/js/script.js
@@ -0,0 +1,21 @@
+//excepptions
+
+var division = function(a, b) {
+    if (typeof b !== 0) {
+        if (typeof a !== 'number' || typeof b !== 'number') {
+            throw {
+                name: 'TypeError',
+                message: 'division needs numbers'
+            };
+        }
+    }
+    return a / b;
+};
+var division_try = function( ) {
+    try {
+        division(7);
+    } catch (e) {
+        document.writeln(e.name + ': ' + e.message);
+    }
+};
+division_try(); // TypeError: division needs numbers
---
+++ Function/js/script.js
@@ -0,0 +1,13 @@
+// Functions
+var addNum = function(num1, num2) {
+    return num1 + num2;
+};
+
+console.log(typeof addNum === 'function' && 'addNum est  une fonction'); //addNum est  une fonction
+
+//A function returns a value. If the return value is not specified, then undefined is returned.
+//
+//If :
+//the function was invoked with the new prefix 
+//and the return value is not an object
+//then this (the new object) is returned instead.
