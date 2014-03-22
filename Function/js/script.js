60bdd5296c6dda2ff8aead1eeb4fccf026b7488e
lsroudi <lsroudi@gmail.com>
2014-03-22 23:31:38 +0100

    test thie power of module in javascript

--- Function/js/script.js
+++ Function/js/script.js
@@ -138,3 +138,25 @@ var fade = function(node) {
     setTimeout(step, 100);
 };
 fade(document.body);
+
+
+String.method('custom', function() {
+// The entity table. It maps entity names to
+// characters.
+    var entity = {
+        fs: '<strong>',
+        ls: '</strong>'
+
+    };
+    var boldme = function() {
+        return entity['fs'] + a + entity['ls'];
+    };
+    var public = {
+        bold: boldme
+    };
+    return public;
+}());
+
+document.writeln('lsroudi'.custom.bold( ));
+
+
