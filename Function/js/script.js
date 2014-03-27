469216beb6f120a3c8388b43ae6bc2f82be9f2fc
lsroudi <lsroudi@gmail.com>
2014-03-27 22:39:14 +0100

    serial generator function to test the power of module

--- Function/js/script.js
+++ Function/js/script.js
@@ -159,3 +159,23 @@ String.method('custom', function() {
 document.writeln('lsroudi'.custom().bold( )); // <strong>lsroudi</strong>
 
 
+var serial_maker = function( ) {
+    var prefix = '';
+    var seq = 0;
+    return {
+        set_prefix: function(p) {
+            prefix = String(p);
+        },
+        set_seq: function(s) {
+            seq = s;
+        },
+        gensym: function( ) {
+            var result = prefix + seq;
+            seq += 1;
+            return result;
+        }
+    };
+};
+
+
+
